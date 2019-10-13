76ffceed67f910709db47a90106ec70c1edeff93
gumiranda <imartytk@hotmail.com>
2019-10-13 10:55:47 -0300

    teste

---
+++ precache-manifest.28a833e97fbc2d917a18cfa8f0da7790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105030bc6adb7df188cad0880aac8fd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b7411b34b2d59006aaa",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "5b7411b34b2d59006aaa",
+    "url": "/static/js/2.911cb7f5.chunk.js"
+  },
+  {
+    "revision": "13167686caf2f9cfb780",
+    "url": "/static/js/main.762ee7ce.chunk.js"
+  },
+  {
+    "revision": "9ac178731e6e473163c8",
+    "url": "/static/js/runtime-main.73b8332d.js"
+  },
+  {
+    "revision": "63c71f84c93f9cd2b6afc2f7ae4d9423",
+    "url": "/static/media/logo.63c71f84.svg"
+  }
+]);
