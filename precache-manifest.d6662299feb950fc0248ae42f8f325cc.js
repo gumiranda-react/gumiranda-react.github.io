518d9f25c72a57aca11b4c4fd5848cee40c7abc0
gumiranda <imartytk@hotmail.com>
2019-10-13 10:49:20 -0300

    initial

---
+++ precache-manifest.d6662299feb950fc0248ae42f8f325cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fd745ea1a6b60c86cf850c7f96519e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "691bbe54a64ba2f8474f",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "691bbe54a64ba2f8474f",
+    "url": "/static/js/2.9e0bf01a.chunk.js"
+  },
+  {
+    "revision": "b1852afa07932000f41b",
+    "url": "/static/js/main.dd880211.chunk.js"
+  },
+  {
+    "revision": "9ac178731e6e473163c8",
+    "url": "/static/js/runtime-main.73b8332d.js"
+  },
+  {
+    "revision": "63c71f84c93f9cd2b6afc2f7ae4d9423",
+    "url": "/static/media/logo.63c71f84.svg"
+  }
+]);
